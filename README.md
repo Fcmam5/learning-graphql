4909be47f6cb882f0a86f759f9c5716a0833f12f
Fcmam5 <dr.fcmam5@gmail.com>
2019-03-15 23:45:29 +0100

    start learning to use prisma

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Learning GraphQL
 
+This repository is my "GraphQL learning notebook" I will share ressources I found helpful for me.
+
 ## Ressources
+
+## Tutorials/Learning-by-doing projects
 * [How to GraphQL: Introduction and basics](https://www.howtographql.com/basics/0-introduction/)
 * [Nodejs example](https://www.howtographql.com/graphql-js/0-introduction/)
     * Exercice: [hackernews-nodejs](./hackernews-nodejs)
+
+## Articles
+* [GraphQL Server Basics: GraphQL Schemas, TypeDefs & Resolvers Explained](https://www.prisma.io/blog/graphql-server-basics-the-schema-ac5e2950214e)
--- hackernews-nodejs/src/index.js
+++ hackernews-nodejs/src/index.js
@@ -1,52 +1,19 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-// 1
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => links.find(l => l.id === args.id),
+    feed: (root, args, context, info) => {
+      return context.prisma.links()
+    },
   },
   Mutation: {
-    // 2
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      }
-      links.push(link)
-      return link
+        description: args.description,
+      })
     },
-    updateLink: (parent, args) => {
-      
-      const index = links.findIndex(l => l.id === args.id);
-      console.log(index);
-      
-      
-      links[index] = {
-        id: args.id,
-        url: args.url || links[index].url,
-        description: args.description || links[index].description,
-      }
-      return links[index];
-    },
-    deleteLink: (parent, args) => {
-      const index = links.findIndex(l => l.id == args.id);
-      const link = links[index];
-      links.splice(index, 1);
-      console.log(links);
-      console.log(index);
-      
-      return link;
-    }
   },
 }
 
